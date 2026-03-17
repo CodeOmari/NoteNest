81fd6cab421c89260b0c73c26eb92c5ffa58a873
CodeOmari <jacksonomari2230@gmail.com>
2026-03-17 12:05:57 +0300

    update the notes page to be responsive on mobile screen sizes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -57,7 +57,7 @@ function Home() {
 
     return (
         <div className="container-fluid">
-            <div className="header d-flex justify-content-between align-items-center">
+            <div className="header d-flex justify-content-between align-items-center pt-3 pb-2">
                 <div className="title-container">
                     <h2 className="title">NoteNest</h2>
                 </div>
