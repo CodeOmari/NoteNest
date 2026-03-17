6eee84e8941c44406f18ec2ab6096615f96fda49
CodeOmari <jacksonomari2230@gmail.com>
2026-03-17 12:43:10 +0300

    update notes listing section to be responsive on different screen sizes

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -97,7 +97,7 @@ function Home() {
                 <input type="submit" value="Submit" className="btn-submit"></input>
             </form>
 
-            <div className="container note-created p-2">
+            <div className="container note-created mt-4">
                 {notes.map((note) => (
                     <Note note={note} onDelete={deleteNote} key={note.id} />
                 ))}
